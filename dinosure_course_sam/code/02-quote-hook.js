9e36da22deb3279544e27a975445330bf1045951
SamBaloyi <SamBaloyi2017@gmail.com>
2024-02-02 19:39:14 +0200

    refactor quote hook

--- dinosure_course_sam/code/02-quote-hook.js
+++ dinosure_course_sam/code/02-quote-hook.js
@@ -17,16 +17,40 @@ const validateQuoteRequest = (data) => {
     data,
     Joi.object()
       .keys({
+        start_date: Joi.date()
+          .iso()
+          .min(moment().startOf('day').toISOString())
+          .max(moment().add(60, 'days').endOf('day').toISOString())
+          .required(),
         cover_amount: Joi.number()
           .integer()
-          .min(100000 * 100)
-          .max(5000000 * 100)
-          .required(),
-        age: Joi.number().integer().min(18).max(63).required(),
-        cardio_fitness_level: Joi.valid(['couch potato', 'marathon runner']).required(),
-        smoker: Joi.boolean().required(),
-        early_warning_network_benefit: Joi.boolean().required(),
-        extraction_benefit: Joi.boolean().required(),
+          .min(10000)
+          .max(100000)
+          .required()
+          .description(
+            'Cover amount must be between R10,000 and R100,000 (in cents) inclusive.',
+          ),
+        birth_date: Joi.date()
+          .min(moment().subtract(50, 'years').startOf('day').toISOString())
+          .max(moment().endOf('day').toISOString())
+          .required()
+          .description(
+            'Date of birth must be within 50 years of the current year.',
+          ),
+        species: Joi.string()
+          .valid(
+            'tyrannosaurus rex',
+            'stegosaurus',
+            'velociraptor',
+            'diplodocus',
+            'iguanodon',
+          )
+          .insensitive()
+          .required()
+          .description(
+            'Valid dinosaur species are Tyrannosaurus Rex, Stegosaurus, Velociraptor, Diplodocus, Iguanodon.',
+          ),
+        health_checks_updated: Joi.boolean().required(),
       })
       .required(),
     { abortEarly: false },
@@ -45,33 +69,16 @@ const getQuote = (data) => {
   // Do the math and logic to calculate the premium, benefits, etc.
   // using `data`, hardcoded rating tables, data stores, or external services/APIs
 
-  const riskPremium = Math.round(calculateRiskPremium(data));
-  const earlyWarningNetworkBenefitPremium = Math.round(calculateEarlyWarningNetworkBenefitPremium(data));
-  const extractionBenefitPremium = Math.round(calculateExtractionBenefitPremium(data));
-
-  const totalPremium = riskPremium + earlyWarningNetworkBenefitPremium + extractionBenefitPremium;
+  const totalPremium = calculatePremium(data);
 
   const quotePackage = new QuotePackage({
     // Below are standard fields for all products
-    package_name: 'Dino protection', // The name of the "package" of cover
+    package_name: 'DinoSure', // The name of the "package" of cover
     sum_assured: data.cover_amount, // Set the total, aggregated cover amount
     base_premium: totalPremium, // Should be an integer, pence
     suggested_premium: totalPremium, // Should be an integer, pence
     billing_frequency: 'monthly', // Can be monthly or yearly
-    module: {
-      // Save any data, calculations, or results here for future re-use.
-      cover_amount: data.cover_amount,
-      age: data.age,
-      cardio_fitness_level: data.cardio_fitness_level,
-      smoker: data.smoker,
-      early_warning_network_benefit: data.early_warning_network_benefit,
-      extraction_benefit: data.extraction_benefit,
-      premium_breakdown: {
-        risk_premium: riskPremium,
-        early_warning_network_benefit_premium: earlyWarningNetworkBenefitPremium,
-        extraction_benefit_premium: extractionBenefitPremium,
-      },
-    },
+    module: data, // Save any data, calculations, or results here for future re-use.
     input_data: { ...data },
   });
 
