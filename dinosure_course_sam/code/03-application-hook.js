8282b05d811f921e3359914c57ca1cc12e1a5995
SamBaloyi <SamBaloyi2017@gmail.com>
2024-02-03 13:09:51 +0200

    add validation schema for application hook

--- dinosure_course_sam/code/03-application-hook.js
+++ dinosure_course_sam/code/03-application-hook.js
@@ -17,11 +17,29 @@ const validateApplicationRequest = (data, policyholder, quote_package) => {
   // Custom validation can be specified in the function body
   const result = Joi.validate(
     data,
-    Joi.object()
-      .keys({
-        // The Joi validation schema can be completed here
-      })
-      .required(),
+    Joi.object().keys({
+      dinosaur_name: Joi.string()
+        .max(100)
+        .required()
+        .description(
+          'The name of the dinosaur must be provided and must be less than 100 characters.',
+        ),
+      dinosaur_colour: Joi.string()
+        .valid('lilac', 'sea green', 'granite grey', 'midnight blue')
+        .insensitive()
+        .required()
+        .description(
+          'The colour of the dinosaur must be one of the following: Lilac, Sea Green, Granite Grey, Midnight Blue.',
+        ),
+      ndrn: Joi.string()
+        .regex(/^\d{6}$/)
+        .required()
+        .min(6)
+        .max(6)
+        .description(
+          'The National Dinosaur Registry Number (NDRN) must be provided and should be a number between 100,000 and 999,999.',
+        ),
+    }),
     { abortEarly: false },
   );
   return result;
