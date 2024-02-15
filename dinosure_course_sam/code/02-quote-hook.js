a8da02d102d36e44d1588cd189d199780221b82c
SamBaloyi <SamBaloyi2017@gmail.com>
2024-02-15 22:56:06 +0200

    add quoteSchema

--- dinosure_course_sam/code/02-quote-hook.js
+++ dinosure_course_sam/code/02-quote-hook.js
@@ -1,6 +1,40 @@
-/**
- * QUOTE HOOK
- */
+const quoteSchema = Joi.object()
+  .keys({
+    start_date: Joi.date()
+      .min(moment().startOf('day').toISOString())
+      .max(moment().add(60, 'days').endOf('day').toISOString())
+      .required(),
+    cover_amount: Joi.number()
+      .integer()
+      .min(10000 * 100)
+      .max(100000 * 100)
+      .required()
+      .description(
+        'Cover amount must be between R10,000 and R100,000 (in cents) inclusive.',
+      ),
+    birth_date: Joi.date()
+      .min(moment().subtract(50, 'years').startOf('day').toISOString())
+      .max(moment().endOf('day').toISOString())
+      .required()
+      .description(
+        'Date of birth must be within 50 years of the current year.',
+      ),
+    species: Joi.string()
+      .valid(
+        'tyrannosaurus rex',
+        'stegosaurus',
+        'velociraptor',
+        'brachiosaurus',
+        'iguanodon',
+      )
+      .insensitive()
+      .required()
+      .description(
+        'Valid dinosaur species are Tyrannosaurus Rex, Stegosaurus, Velociraptor, Brachiosaurus, Iguanodon.',
+      ),
+    health_checks_updated: Joi.boolean().required(),
+  })
+  .required();
 
 /**
  * Validates the quote request data.
@@ -13,47 +47,7 @@
  */
 const validateQuoteRequest = (data) => {
   // Custom validation can be specified in the function body
-  const result = Joi.validate(
-    data,
-    Joi.object()
-      .keys({
-        start_date: Joi.date()
-          .min(moment().startOf('day').toISOString())
-          .max(moment().add(60, 'days').endOf('day').toISOString())
-          .required(),
-        cover_amount: Joi.number()
-          .integer()
-          .min(10000 * 100)
-          .max(100000 * 100)
-          .required()
-          .description(
-            'Cover amount must be between R10,000 and R100,000 (in cents) inclusive.',
-          ),
-        birth_date: Joi.date()
-          .min(moment().subtract(50, 'years').startOf('day').toISOString())
-          .max(moment().endOf('day').toISOString())
-          .required()
-          .description(
-            'Date of birth must be within 50 years of the current year.',
-          ),
-        species: Joi.string()
-          .valid(
-            'tyrannosaurus rex',
-            'stegosaurus',
-            'velociraptor',
-            'brachiosaurus',
-            'iguanodon',
-          )
-          .insensitive()
-          .required()
-          .description(
-            'Valid dinosaur species are Tyrannosaurus Rex, Stegosaurus, Velociraptor, Brachiosaurus, Iguanodon.',
-          ),
-        health_checks_updated: Joi.boolean().required(),
-      })
-      .required(),
-    { abortEarly: false },
-  );
+  const result = Joi.validate(data, quoteSchema, { abortEarly: false });
   return result;
 };
 
@@ -65,9 +59,6 @@ const validateQuoteRequest = (data) => {
  * @see {@link https://docs.rootplatform.com/docs/quote-hook Quote hook}
  */
 const getQuote = (data) => {
-  // Do the math and logic to calculate the premium, benefits, etc.
-  // using `data`, hardcoded rating tables, data stores, or external services/APIs
-
   const totalPremium = calculatePremium(data);
 
   const quotePackage = new QuotePackage({
